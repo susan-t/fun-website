fdbf79fad15e4db691040fa2dd270e564c284785
susan-t <susan.thao@outlook.com>
2026-06-03 00:34:19 -0500

    fix formatting

--- script.js
+++ script.js
@@ -1,92 +1,52 @@
-// CHANGE THIS TO YOUR DATE
-const correctCode = "120825";
+const correctCode = "040825";
 
 let input = "";
 
-const numberButtons =
-  document.querySelectorAll(".num");
-
-const hearts =
-  document.querySelectorAll(".heart");
-
-const backspace =
-  document.getElementById("backspace");
-
-const enterBtn =
-  document.getElementById("enterBtn");
-
-
+const numberButtons = document.querySelectorAll(".num");
+const hearts = document.querySelectorAll(".heart");
+const backspace = document.getElementById("backspace");
+const enterBtn = document.getElementById("enterBtn");
 
 // NUMBER BUTTONS
 numberButtons.forEach(button => {
-
   button.addEventListener("click", () => {
-
     if(input.length < 6){
-
       input += button.textContent;
-
       updateHearts();
-
     }
-
   });
-
 });
 
 
 
 // UPDATE HEARTS
 function updateHearts(){
-
-  hearts.forEach((heart, index) => {
-
-    if(index < input.length){
-
-      heart.textContent = "❤";
-      heart.classList.add("filled");
-
-    }
-    else{
-
-      heart.textContent = "♡";
-      heart.classList.remove("filled");
-
-    }
-
-  });
-
+    hearts.forEach((heart, index) => {
+        if(index < input.length){
+            heart.textContent = "❤";
+            heart.classList.add("filled");
+        }
+        else{
+            heart.textContent = "♡";
+            heart.classList.remove("filled");
+        }
+    });
 }
-
-
 
 // BACKSPACE
 backspace.addEventListener("click", () => {
-
-  input = input.slice(0, -1);
-
-  updateHearts();
-
+    input = input.slice(0, -1);
+    updateHearts();
 });
-
-
 
 // ENTER BUTTON
 enterBtn.addEventListener("click", () => {
-
-  if(input === correctCode){
-
-    window.location.href = "nextpage.html";
-
-  }
-  else{
-
-    alert("wrong code :(");
-
-    input = "";
-
-    updateHearts();
-
-  }
-
+    if(input === correctCode){
+        window.location.href = "nextpage.html";
+    }
+    else{
+        alert("wrong code :(");
+        input = "";
+        updateHearts();
+    }
 });
